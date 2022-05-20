d80f05f6f42eb446ab1fe8c9fee0b93f31a45e5c
M H R P <mh17pwfc@gmail.com>
2022-05-21 00:49:56 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+      grape : ['Grape Nuts'],
+      poppins :['Poppins'],
+      }
+    },
+  },
+  plugins: [],
+}
